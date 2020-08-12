1ace7ce983542213b2c30a369021ce8ba20d9c5a
Vakabunga <shesterov@gmail.com>
2020-08-12 22:38:36 +0300

    finish game without classes. TODO: devide app.js to classes

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,18 @@
 // Задаем размер доски [row, column]
 const boardSize = [4, 4];
-document.querySelector('body').setAttribute('style', `width: ${boardSize[1] * 120 + boardSize[1] * 6}px`);
+const body = document.querySelector('body');
+body.setAttribute('style', `width: ${boardSize[1] * 120 + boardSize[1] * 6}px`);
+
+body.insertAdjacentHTML(
+  'beforebegin',
+  `<h1>The Game of the Year</h1>
+  <div class="player" style="width: ${boardSize[1] * 120 + boardSize[1] * 6}px">
+  <div class="playerName">Player Name: <span class="playerName">Player1</span></div>
+  <div class="playerScore">Score: <span class="score">0</span></div>
+  </div>`
+);
+
+const score = document.querySelector('.score');
 
 // функция для отрисовки доски, входной параметр - массив из двух данных
 export default function boardInit(size) {
@@ -16,7 +28,9 @@ export default function boardInit(size) {
 
 boardInit(boardSize);
 
+let playerScore = 0;
 const cell = document.querySelectorAll('.cell');
+const board = document.querySelector('.board');
 let oldGoblin = 0;
 
 setInterval(() => {
@@ -31,3 +45,27 @@ setInterval(() => {
   oldGoblin = goblin;
   cell[goblin].classList.add('active');
 }, 1000);
+
+board.addEventListener('mousedown', (event) => {
+  if (event.target.classList.contains('cell')) {
+    event.target.classList.add('clicked');
+  }
+});
+
+board.addEventListener('mouseup', (event) => {
+  if (event.target.classList.contains('clicked')) {
+    event.target.classList.remove('clicked');
+  }
+});
+
+board.addEventListener('click', (event) => {
+  if (event.target.classList.contains('active')) {
+    playerScore += 1;
+    score.textContent = playerScore;
+    event.target.classList.remove('active');
+  } else {
+    playerScore = 0;
+    score.textContent = playerScore;
+    alert('Game Over');
+  }
+});
